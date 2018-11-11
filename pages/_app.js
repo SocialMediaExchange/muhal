ed76c7c4d963836e3e22bb060c093752fc4104a5
Marc Farra <marcfarra@gmail.com>
2018-11-11 13:28:14 -0500

    Move sidebar for now

--- pages/_app.js
+++ pages/_app.js
@@ -1,23 +1,8 @@
 import Head from 'next/head'
 import Link from 'next/link'
 import App, {Container} from 'next/app'
-import { Timeline } from 'react-twitter-widgets'
 import React from 'react'
-import NoSSR from 'react-no-ssr'
 
-const Loading = () => (
-  <div>
-    <h3>Loading...</h3>
-    <style jsx>{`
-      div {
-        align-items: center;
-        display: flex;
-        height: 50vh;
-        justify-content: center;
-      }
-    `}</style>
-  </div>
-)
 
 const Layout = ({ children }) => (
   <div class="sans-serif">
@@ -34,9 +19,9 @@ const Layout = ({ children }) => (
           <nav class="db dt-l w-100 border-box">
             <a class="db dtc-l v-mid b black f3 link w-100 w-33-l tc tl-l mb2 mb0-l tracked-tight" href="/" title="Home">Freedom of Speech in Lebanon</a>
             <div class="db dtc-l v-mid w-100 w-75-l tc tr-l">
-              <a class="link dim black f6 ttu f5-l dib mr3 mr4-l" href="/" title="Cases">Cases</a>
-              <a class="link dim black f6 ttu f5-l dib mr3 mr4-l" href="/about" title="About">About</a>
-              <a class="link dim white bg-light-red br2 pa2 f6 f5-l dib b ttu" href="/report" title="Report">Report Issue</a>
+              <Link><a class="link dim black f6 ttu f5-l dib mr3 mr4-l" href="/" title="Cases">Cases</a></Link>
+              <Link><a class="link dim black f6 ttu f5-l dib mr3 mr4-l" href="/about" title="About">About</a></Link>
+              <Link><a class="link dim white bg-light-red br2 pa2 f6 f5-l dib b ttu" href="/report" title="Report">Report Issue</a></Link>
             </div>
           </nav>
         </header>
@@ -44,22 +29,6 @@ const Layout = ({ children }) => (
           {children}
         </div>
       </segment>
-      <footer class="pa3-l pa2 center w-30">
-        <h6 class="i tc f7 dark-gray avenir lh-copy mv0">2018 SMEX.org · <a href="https://creativecommons.org/licenses/by/4.0/">CC BY 4.0</a></h6>
-        <NoSSR onSSR={<Loading />} class="center pa3-l pa2 dib-l dn">
-          <Timeline
-            dataSource={{
-              sourceType: 'profile',
-              screenName: 'SMEX'
-            }}
-            options={{
-              username: 'smex',
-              height: '500'
-            }}
-            onLoad={() => console.log('Timeline is loaded!')}
-          />
-        </NoSSR>
-      </footer>
     </div>
   </div>
 )
--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,39 @@
 import React from 'react'
+import { Timeline } from 'react-twitter-widgets'
+import NoSSR from 'react-no-ssr'
+
+const Loading = () => (
+  <div>
+    <h3>Loading...</h3>
+    <style jsx>{`
+      div {
+        align-items: center;
+        display: flex;
+        height: 50vh;
+        justify-content: center;
+      }
+    `}</style>
+  </div>
+)
 
 export default () => {
   return (
+    <div>
     <h1>About</h1>
+    <footer class="pa3-l pa2 w-30">
+      <NoSSR onSSR={<Loading />} class="center pa3-l pa2 dib-l dn">
+        <Timeline
+          dataSource={{
+            sourceType: 'profile',
+            screenName: 'SMEX'
+          }}
+          options={{
+            username: 'smex',
+            height: '500'
+          }}
+        />
+      </NoSSR>
+    </footer>
+    </div>
   )
 }
